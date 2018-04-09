5e2b8da1654d292c090b7ca9778eb27dd689370d
Jimmy Delaney <jimmydelaney19@gmail.com>
2018-04-08 21:40:06 -0500

    trying to get babylon working

--- public/babylon/main.js
+++ public/babylon/main.js
@@ -1,6 +1,53 @@
 // Main Controller Code
 var socket = io();
 
+var canvas = document.getElementById("canvas");
+var engine = new BABYLON.Engine(canvas, true);
+
+var createScene = function(){
+    // This creates a basic Babylon Scene object (non-mesh)
+    var scene = new BABYLON.Scene(engine);
+
+    // This creates and positions a free camera (non-mesh)
+    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+
+    // This targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+    // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
+    //var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+
+    // Move the sphere upward 1/2 its height
+    //sphere.position.y = 1;
+
+    // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
+    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+
+    return scene;
+}
+
+var scene = createScene();
+
+BABYLON.SceneLoader.ImportMesh("basketball", "", "basketball3d.obj", scene, function (mesh) {
+    // do something with the meshes and skeletons
+    // particleSystems are always null for glTF assets
+    camera.target = mesh;
+});
+
+engine.runRenderLoop(function(){
+    scene.render();
+});
+
+
 var $window = $(window);
 var $pages = $('.pages'); // Input for roomname
 var $passcodeInput = $('.passcodeInput'); // Input for roomname
