1a0f3782d40896dd2d7c4acfc4e82796e11b3f0b
Jimmy Delaney <jimmydelaney@Jimmys-MacBook-Air.local>
2018-04-13 12:14:40 -0500

    light, cannon, close

--- public/babylon/main.js
+++ public/babylon/main.js
@@ -61,10 +61,10 @@ var createScene = function(){
     camera.attachControl(canvas, true);
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    //var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
     // Default intensity is 1. Let's dim the light a small amount
-    //light.intensity = 0.7;
+    light.intensity = 0.7;
 
     var basketball = BABYLON.Mesh.CreateSphere("basketball", 16, 1.88, scene);
 
