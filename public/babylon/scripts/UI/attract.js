f1291ac3baa712f0b3cadd63a54b64bfcf08f6d9
Jimmy Delaney <jimmydelaney19@gmail.com>
2018-05-02 19:48:32 -0500

    UI beginnings

---
+++ public/babylon/scripts/UI/attract.js
@@ -0,0 +1,12 @@
+function UIAttractAnimateIn()
+{
+    //BBALL Text instructions 1+2 appear in bottom
+    //Show background Graphic 1 and background Graphic 2
+}
+
+function UIAttractAnimateOut()
+{
+    //Fade out both bball texts
+    //Fade out Play Now!
+    //Call Waitings animateIn
+}
