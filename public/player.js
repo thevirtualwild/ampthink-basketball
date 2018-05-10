10e18c42477815a45e5c55230924a5c741e462b6
Jimmy Delaney <jimmydelaney19@gmail.com>
2018-05-10 07:44:49 -0500

    no message

--- public/player.js
+++ public/player.js
@@ -158,7 +158,8 @@ function createScene() {
 
           pageScaleFactorX = 1;
           pageScaleFactorY = screen.height/height;
-            console.log("SCALE FACTOR: " + pageScaleFactorY);
+          consoel.log(ev);
+           // console.log("SCALE FACTOR: " + pageScaleFactorY);
           if(currentBallState != ballStates.DRAGGING) return;
           targetX = ev.pageX * pageScaleFactorX;
           targetY = ev.pageY * pageScaleFactorY;
