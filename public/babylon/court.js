fe2814fa8f5deef014f1cf72a0ca351f6ee11920
Jimmy Delaney <jimmydelaney@jimmys-air.attlocal.net>
2018-05-15 09:54:17 -0500

    made master bar very tiny

--- public/babylon/court.js
+++ public/babylon/court.js
@@ -1479,7 +1479,7 @@ engine.runRenderLoop(function() {
     if(ISMASTER)
     {
         fpsLabel.style.background = "red";
-        fpsLabel.style.height = "100%";
+        fpsLabel.style.height = "2px";
     }
 
     //scene.getPhysicsEngine().setTimeStep(1/(engine.getFps() * .75));
